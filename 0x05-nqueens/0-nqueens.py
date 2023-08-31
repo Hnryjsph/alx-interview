7e13ef9a30136a6cd0ab3698976d31ea4e931ecb
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2023-08-31 19:58:56 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,32 +4,39 @@
 import sys
 
 def is_safe(board, row, col, N):
-    for i in range(col):
-        if board[row][i] == 1:
+    # Check if there's a queen in the same column
+    for i in range(row):
+        if board[i][col] == 1:
             return False
-        
+    
+    # Check upper left diagonal
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-        
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+    
+    # Check upper right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, N)):
         if board[i][j] == 1:
             return False
-        
+    
     return True
 
-def solve_nqueens(board, col, N):
-    if col >= N:
-        for row in board:
-            print([row.index(1), col - N + 1])
-        print()
-        return
+def solve_nqueens(N):
+    def backtrack(row):
+        nonlocal solutions
+        if row == N:
+            solutions.append([[r, c] for r, c in enumerate(board)])
+            return
+        for col in range(N):
+            if is_safe(board, row, col, N):
+                board[row][col] = 1
+                backtrack(row + 1)
+                board[row][col] = 0
     
-    for i in range(N):
-        if is_safe(board, i, col, N):
-            board[i][col] = 1
-            solve_nqueens(board, col + 1, N)
-            board[i][col] = 0
+    solutions = []
+    board = [[0] * N for _ in range(N)]
+    backtrack(0)
+    return solutions
 
 def main():
     if len(sys.argv) != 2:
@@ -46,8 +53,10 @@ def main():
         print("N must be at least 4")
         sys.exit(1)
     
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    solve_nqueens(board, 0, N)
+    solutions = solve_nqueens(N)
+    for solution in solutions:
+        print(solution)
 
 if __name__ == "__main__":
     main()
+
